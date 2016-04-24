68dafa5d693191d72e92fce1a3e833c4249dacf4
yunxiye <yunxiye@gmail.com>
2016-04-24 15:22:02 +0800

    add src

---
+++ src/github.com/YuncyYe/hello.go
@@ -0,0 +1,17 @@
+/*
+e:\projects\go\go_learning>set GOPATH=E:\projects\go\go_learning\
+
+e:\projects\go\go_learning>set GOBIN=E:\projects\go\go_learning\bin
+e:\projects\go\go_learning\src>go install github.com\YuncyYe\hello.go
+e:\projects\go\go_learning\src>.\..\bin\hello.exe
+Hello, world.
+
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, world.\n")
+}
+
