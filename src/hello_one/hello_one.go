bc72f18a82952808715b8f56ed0f868a2f248e67
yunxiye <yunxiye@gmail.com>
2016-04-24 16:07:12 +0800

    update

--- src/hello_one/hello_one.go
+++ src/hello_one/hello_one.go
@@ -1,5 +1,5 @@
 /*
-e:\projects\go\go_learning>set GOPATH=E:\projects\go\go_learning\
+e:\projects\go\go_learning>set GOPATH=E:\projects\go\go_learning
 
 e:\projects\go\go_learning>set GOBIN=E:\projects\go\go_learning\bin
 e:\projects\go\go_learning\src>go install github.com\YuncyYe\hello.go
